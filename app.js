c330eb79684b1e2f7385cf4d4720a5f77ddec15b
WindowsROOT <greanner@gmail.com>
2020-10-16 11:18:07 +0700

    This is a new commit for what I originally planned to be amended

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+var port = 4000;
+
+app.get("/", (req, res) => {
+  res.send({
+    status: "200"});
+});
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
